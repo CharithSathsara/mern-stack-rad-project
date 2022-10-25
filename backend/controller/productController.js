3c110e7e69fffee0d7babc318634f3592fa5605a
Charith Sathsara <charithsathsara169@gmail.com>
2022-10-25 22:54:34 +0530

    updating product back end part

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -94,6 +94,8 @@ const deleteProduct = asyncHandler( async (req, res) => {
         throw new Error('Goal not found');
     }
 
+
+
     //check for user
     if(!req.user){
         res.status(401)
