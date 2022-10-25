4c04cfaee8ee8a6ba2f4399fa3b2f56b7e1efd29
Charith Sathsara <charithsathsara169@gmail.com>
2022-10-25 23:18:29 +0530

    updating server.js file

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+//console.log("Entry point of our application");
+//const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+
+const connectDB = require('./config/db');
+
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+//app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+//app.use(cors);
+
+app.use('/api/users',require('./routes/userRoutes'));
+
+// Products
+app.use('/api/products',require('./routes/productRoutes'));
+// Customers
+app.use('/api/customers',require('./routes/customerRoutes'));
+// Income
+app.use('/api/income',require('./routes/incomeRoutes'));
+// Distributors
+app.use('/api/distributors',require('./routes/distributorRoutes'));
+// Expenditures
+app.use('/api/expenditures',require('./routes/expenditureRoutes'));
+
+//override the default error handler
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
